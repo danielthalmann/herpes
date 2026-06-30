579903578fbb8029e4b19f0865afb5b1956ae9ad
Daniel Thalmann <daniel@thalmann.li>
2026-06-30 22:51:58 +0200

    add new table tickets and timesheets

---
+++ packages/danielthalmann/herpes/database/migrations/2026_06_30_201334_create_tickets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('type')->nullable();
+            $table->string('status')->nullable();
+            $table->string('summary')->nullable();
+            $table->text('description')->nullable();
+            $table->uuid('customer_id')->nullable();
+            $table->uuid('parent_id')->nullable();
+            $table->integer('reporter_id')->nullable();
+            $table->integer('assignee_id')->nullable();
+            $table->boolean('invoice')->default(0)->nullable();
+            $table->dateTime('invoiced_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
