e0db7c38398d6de77339f6f6fbf0b2a605b33092
Daniel Thalmann <daniel@thalmann.li>
2026-06-30 23:50:29 +0200

    dialog scroll

--- packages/danielthalmann/herpes/resources/js/components/Dialog.svelte
+++ packages/danielthalmann/herpes/resources/js/components/Dialog.svelte
@@ -16,15 +16,6 @@
     } : DialogProps = $props();
 
 
-    let value = $state('');
-    let stateClass = $state('');
-
-    $effect(() => {
-        if (!open) {
-            stateClass = "";
-            value = "";
-        }
-    });
 
 </script>
 
@@ -33,24 +24,23 @@
     <div class="portal">
         <div class="overlay z-2000 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 bg-black/50"  transition:fade
         ></div>
-        <div
-            class="bg-gray-300 dark:bg-gray-800/90 rounded-lg shadow-2xl data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 outline-hidden fixed left-[50%] top-[50%] z-2050 w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] border p-5 sm:max-w-140 md:w-full"  transition:fade
-        >
+        <div class="max-h-lvh top-0 bottom-0 relative overflow-y-visible">
             <div
-                class="flex w-full items-center justify-center text-lg font-semibold tracking-tight"
+                class="bg-gray-300 dark:bg-gray-800/90 rounded-lg shadow-2xl data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 outline-hidden fixed left-[50%] top-[50%] z-2050 w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] border sm:max-w-140 md:w-full flex flex-col max-h-[90vh]"  transition:fade
             >
-                {#if title}
-                    {title}
-                {:else}
-                    {'Dialog'}
-                {/if}
-            </div>
-            <div class="bg-muted -mx-5 mb-6 mt-5 block h-px border-b"
-            ></div>
-            <div class="text-foreground-alt">
-
-                {@render children?.()}
-
+                <div class="shrink-0 px-5 pt-5">
+                    <div class="flex w-full items-center justify-center text-lg font-semibold tracking-tight">
+                        {#if title}
+                            {title}
+                        {:else}
+                            {'Dialog'}
+                        {/if}
+                    </div>
+                    <div class="bg-muted -mx-5 mb-0 mt-5 block h-px border-b"></div>
+                </div>
+                <div class="text-foreground-alt overflow-y-auto flex-1 px-5 py-5">
+                    {@render children?.()}
+                </div>
             </div>
         </div>
     </div>
